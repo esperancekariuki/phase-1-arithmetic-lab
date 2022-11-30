af1ddd14ff262e786b201ecd8b43e7cbb941379f
esperancekariuki <kariukiesperance@gmail.com>
2022-11-30 17:03:33 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 62
+let num2 = 1
+const multiply = num1 * num2
+   multiply;
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 22
+let num4 = 6
+const mod = (num3 % num4)
+mod;
+
+let max = Math.max(20,4)
+
+
+  
 
 // Write your code here
